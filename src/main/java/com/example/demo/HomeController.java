80082250cf12c4ab7eef30568c2941cef1e2f5c0
katcampbell18 <ktrncampbell@gmail.com>
2019-08-22 18:18:06 -0400

    initial

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/")
+    public String listPosts(Model model) {
+        model.addAttribute("posts", postRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String postForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "postform";
+    }
+
+    @PostMapping("/add")
+    public String processPost(@ModelAttribute Post post, @RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "redirect:/add";
+        }
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(), ObjectUtils.asMap("resourcetype", "auto"));
+            post.setImage(uploadResult.get("url").toString());
+            postRepository.save(post);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "redirect:/add";
+        }
+        return "redirect:/";
+    }
+    @RequestMapping("detail/{id}")
+    public String showPost(@PathVariable("id") long id, Model model){
+        model.addAttribute("post", postRepository.findById(id).get());
+        return "show";
+    }
+
+}
+
+
+
---
+++ src/main/java/com/example/demo/Post.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=3, max=120)
+    private String content;
+
+    @NotNull
+    private Date postedDate;
+
+    @NotNull
+    @Size(min=4, max=20)
+    private String sentBy;
+
+    private String image;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(Date postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
